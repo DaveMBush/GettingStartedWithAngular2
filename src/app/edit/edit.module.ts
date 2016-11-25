211234e816543f5d3caece70d3921277901355e5
davembush <davembush@dmbcllc.com>
2016-11-25 12:41:06 -0500

    Added routing

---
+++ src/app/edit/edit.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditComponent } from './edit.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EditComponent]
+})
+export class EditModule { }
---
+++ src/app/view/view.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewComponent } from './view.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ViewComponent]
+})
+export class ViewModule { }
